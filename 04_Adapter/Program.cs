42ff02d48a265c311e41b9ec420dfb4606534bfc
Wayne <wayneeeeee@gmail.com>
2020-07-12 11:26:45 +0800

    Add adapter pattern.

---
+++ 04_Adapter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04_Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter adapter = new Adapter(new Adaptee());
+            adapter.Request();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ITarget
+    {
+        void Request();
+    }
+
+    class Adaptee
+    {
+        public void AdapteeRequest()
+        {
+            Console.WriteLine("Hello Adaptee!!");
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        protected Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public void Request()
+        {
+            if (_adaptee != null)
+                _adaptee.AdapteeRequest();
+        }
+    }
+
+}
+
+
