e1300adf0eb2eaf22b77dc485a93329068a8b76c
Wayne <wayneeeeee@gmail.com>
2020-07-18 19:59:13 +0800

    Add Behavioral_Template pattern.

---
+++ 08_Behavioral_Template/Callback.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _08_Behavioral_Template
+{
+    public class Callback
+    {
+        public void Run()
+        {
+            Console.WriteLine($"Method Run，執行緒ID: {Thread.CurrentThread.ManagedThreadId}");
+
+            Async_Callback asyncCallback = new Async_Callback();
+            
+            asyncCallback.OnCompletion += (sender, eventArgs) =>
+                                          {
+                                              Console.WriteLine("---");
+                                              Console.WriteLine($"callback 開始，執行緒ID: {Thread.CurrentThread.ManagedThreadId}");
+                                              Console.WriteLine("callback 結束");
+                                              Console.WriteLine("---");
+                                          };
+            
+            Console.WriteLine($"DoSomething 開始，執行緒ID: {Thread.CurrentThread.ManagedThreadId}");
+            
+            asyncCallback.DoSomething();
+
+            Console.WriteLine($"DoSomething 結束，執行緒ID: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.ReadLine();
+        }
+
+        public class Async_Callback
+        {
+            public EventHandler OnCompletion;
+            public void DoSomething()
+            {
+                Console.WriteLine($"DoSomething，執行緒ID: {Thread.CurrentThread.ManagedThreadId}");
+                Task.Run(() =>
+                         {
+                             Console.WriteLine($"new Task 呼叫 callback，執行緒ID: {Thread.CurrentThread.ManagedThreadId}");
+                             OnCompletion?.Invoke(this, EventArgs.Empty);
+                         }).Wait();
+            }
+        }
+    }
+
+    
+}
---
+++ 08_Behavioral_Template/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _08_Behavioral_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Template_Run();
+
+            Callback callback = new Callback();
+            callback.Run();
+
+            Console.ReadLine();
+        }
+
+        private static void Template_Run()
+        {
+            AbstractClass aA = new Class_A();
+            aA.TemplateMethod();
+
+            AbstractClass aB = new Class_B();
+            aB.TemplateMethod();
+        }
+
+        /// <summary>
+        /// 將共用部分抽出至父類別
+        /// </summary>
+        abstract class AbstractClass
+        {
+            public abstract void FirstOperation();
+            public abstract void SecondOperation();
+
+            // Template method
+            public void TemplateMethod()
+            {
+                FirstOperation();
+                SecondOperation();
+                Console.WriteLine("");
+            }
+        }
+
+        /// <summary>
+        /// 實作 AbstractClass 的 Class_A
+        /// </summary>
+        class Class_A : AbstractClass
+        {
+            public override void FirstOperation()
+            {
+                Console.WriteLine("Class_A, FirstOperation()");
+            }
+            public override void SecondOperation()
+            {
+                Console.WriteLine("Class_A, SecondOperation()");
+            }
+        }
+
+        /// <summary>
+        /// 實作 AbstractClass 的 Class_B
+        /// </summary>
+        class Class_B : AbstractClass
+        {
+            public override void FirstOperation()
+            {
+                Console.WriteLine("Class_B, FirstOperation()");
+            }
+            public override void SecondOperation()
+            {
+                Console.WriteLine("Class_B, SecondOperation()");
+            }
+        }
+    }
+}
