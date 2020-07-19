1345e9203ba57bb2a138e3d826a4c551f2c410de
Wayne <wayneeeeee@gmail.com>
2020-07-19 15:48:27 +0800

    Add Creational_01_Singleton pattern.

---
+++ Creational_01_Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Creational_01_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = Singleton.GetInstance();
+            var s2 = Singleton.GetInstance();
+
+            Console.WriteLine(s1 == s2 ? "實體相同。" : "實體不同。");
+
+            Console.ReadLine();
+        }
+
+        class Singleton
+        {
+            private static Singleton _instance;
+
+            // 避免由外部直接 new 出，將建構子設為 private
+            private Singleton()
+            {
+            }
+
+            public static Singleton GetInstance()
+            {
+                // not thread safety.
+                return _instance ??= new Singleton();
+            }
+        }
+    }
+}
