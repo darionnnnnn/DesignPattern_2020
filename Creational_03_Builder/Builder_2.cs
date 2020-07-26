555ed244495757438c19ee203a3e956201876262
Wayne <wayneeeeee@gmail.com>
2020-07-26 23:21:17 +0800

    Add Creational_Builder pattern.

---
+++ Creational_03_Builder/Builder_2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creational_03_Builder
+{
+    public class Builder_2
+    {
+        public void Run()
+        {
+            CustomerBuilder builder = new CustomerBuilder();
+
+            var customer = builder.SetUserName("Wayne")
+                                  .SetEMail("Wayne@gmail.com")
+                                  .SetPhoneNumber("0912345678")
+                                  .GetConcrete();
+
+            customer.Run();
+
+            Console.ReadLine();
+        }
+
+        class CustomerBuilder
+        {
+            private string _UserName { get; set; }
+            private string _EMail { get; set; }
+            private string _PhoneNumber { get; set; }
+
+            public CustomerBuilder SetUserName(string userName)
+            {
+                _UserName = userName;
+
+                return this;
+            }
+
+            public CustomerBuilder SetEMail(string email)
+            {
+                _EMail = email;
+
+                return this;
+            }
+            public CustomerBuilder SetPhoneNumber(string phoneNumber)
+            {
+                _PhoneNumber = phoneNumber;
+
+                return this;
+            }
+
+            public Customer GetConcrete()
+            {
+                return new Customer(_UserName, _EMail, _PhoneNumber);
+            }
+        }
+
+        class Customer
+        {
+            private string _UserName { get; }
+            private string _EMail { get; }
+            private string _PhoneNumber { get; }
+
+            public Customer(string userName, string email, string phoneNumber)
+            {
+                _UserName = userName;
+                _EMail = email;
+                _PhoneNumber = phoneNumber;
+            }
+
+            public void Run()
+            {
+                Console.WriteLine($"UserName : {_UserName}");
+                Console.WriteLine($"EMail : {_EMail}");
+                Console.WriteLine($"PhoneNumber : {_PhoneNumber}");
+            }
+        }
+    }
+}
---
+++ Creational_03_Builder/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace Creational_03_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            Console.WriteLine("-- 1");
+            Run();
+
+            // 2
+            Console.WriteLine("\n-- 2");
+            var builder2 = new Builder_2();
+            builder2.Run();
+
+            Console.ReadLine();
+        }
+
+        static void Run()
+        {
+            Director director = new Director();
+
+            IBuilder builder1 = new ConcreteBuilder1();
+            IBuilder builder2 = new ConcreteBuilder2();
+
+            director.Construct(builder1);
+            Product product1 = builder1.GetProduct();
+            product1.Show();
+
+            director.Construct(builder2);
+            Product product2 = builder2.GetProduct();
+            product2.Show();
+        }
+
+        class Director
+        {
+            public void Construct(IBuilder builder)
+            {
+                builder.BuildFirstPart();
+                builder.BuildSecondPart();
+            }
+        }
+
+        interface IBuilder
+        {
+            void BuildFirstPart();
+            void BuildSecondPart();
+            Product GetProduct();
+        }
+
+        class ConcreteBuilder1 : IBuilder
+
+        {
+            private Product _product = new Product();
+
+            public void BuildFirstPart()
+            {
+                _product.Add("Part A");
+            }
+
+            public void BuildSecondPart()
+            {
+                _product.Add("Part B");
+            }
+
+            public Product GetProduct()
+            {
+                return _product;
+            }
+        }
+
+        class ConcreteBuilder2 : IBuilder
+        {
+            private Product _product = new Product();
+
+            public void BuildFirstPart()
+            {
+                _product.Add("Part 1");
+            }
+
+            public void BuildSecondPart()
+            {
+                _product.Add("Part 2");
+            }
+
+            public Product GetProduct()
+            {
+                return _product;
+            }
+        }
+
+        class Product
+        {
+            private List<string> _parts = new List<string>();
+
+            public void Add(string part)
+            {
+                _parts.Add(part);
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("\nProduct Parts -------");
+
+                foreach (var part in _parts)
+                {
+                    Console.WriteLine(part);
+                }
+            }
+        }
+    }
+}
